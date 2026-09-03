130aae2edaad88cf23688c6ef829677cb6559e6b
Avi <avilionamillion@gmail.com>
2026-09-03 17:11:34 -0500

    a1.1.3
    Fixed an issue with harvest levels for tools being incorrect.
    Fixed quests to use raw ore instead of regular ore.

--- groovy/preInit/gt_materials.groovy
+++ groovy/preInit/gt_materials.groovy
@@ -8,21 +8,28 @@ event_manager.listen { PostMaterialEvent event ->
  nickelbyproducts.add(material('iron'))
  nickelbyproducts.add(material('cobalt'))
  nickelbyproducts.add(material('platinum_crude'))
-}
 
-event_manager.listen { PostMaterialEvent event ->
  def bauxite = material('bauxite').getProperty(PropertyKey.ORE)
  def bxbyproducts = bauxite.getOreByProducts()
  bxbyproducts.clear()
  bxbyproducts.add(material('bauxilmenite'))
  bxbyproducts.add(material('gallium'))
  bxbyproducts.add(material('gallium'))
-}
-event_manager.listen { PostMaterialEvent event ->
+
  def ilmenite = material('ilmenite').getProperty(PropertyKey.ORE)
  def ilmenitebyproducts = ilmenite.getOreByProducts()
  ilmenitebyproducts.clear()
  ilmenitebyproducts.add(material('bauxilmenite'))
  ilmenitebyproducts.add(material('bauxilmenite'))
  ilmenitebyproducts.add(material('iron'))
+
+ def ironTL = material('iron').getProperty(PropertyKey.TOOL)
+ ironTL.setToolHarvestLevel(3)
+
+ def steelTL = material('steel').getProperty(PropertyKey.TOOL)
+ steelTL.setToolHarvestLevel(5)
+
+ def wironTL = material('wrought_iron').getProperty(PropertyKey.TOOL)
+ wironTL.setToolHarvestLevel(4)
+
 }
